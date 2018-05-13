eb83f31c22a21d7d525063fa0ea0c5e283eba6d9
kamilzegadlo <kamilzegadlo@gmail.com>
2018-05-14 00:59:47 +0100

    testing calling http

--- src/app/image.service.spec.ts
+++ src/app/image.service.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
-import { ImageService } from './barrel';
+import { ImageService, Place } from './barrel';
 
 describe('ImageService', () => {
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ImageService, HttpClient],
-      imports: [HttpClientModule]
+      providers: [ImageService],
+      imports: [HttpClientTestingModule]
     });
+
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it(
@@ -19,13 +27,17 @@ describe('ImageService', () => {
     })
   );
 
-  xit(
+  it(
     'should return a mocked place',
     inject([ImageService], (service: ImageService) => {
-      const place = service.getPlace({ x: 13, y: 14 });
+      service.getPlace({ x: 13, y: 14 }).subscribe(place => {
+        //expect(place.x).toBe(13);
+        //expect(place.y).toEqual(14);
+      });
 
-      expect(place.x).toBe(13);
-      expect(place.y).toBe(14);
+      let placeRequest = httpMock.expectOne('api/place');
+      placeRequest.flush({} as Place);
+      httpMock.verify();
     })
   );
 });
