7d4a331114555f39ba9a061306da86a60fa2f38b
Kamil Zegadlo <kamilzegadlo@gmail.com>
2018-11-14 20:32:47 +0000

    These references to in memory web api should have been already removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
